816f0a3ebf9c7e9974ccfacc2a5723afa5f0f1c9
maxcaetanoo <maxsuelcaetano@live.com>
2024-02-29 14:50:46 -0300

    else if

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 65
+
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18){
+    console.log('Voto opcional')
+} else if (idade < 65){
+    console.log('Voto obrigatório')
+} else {
+    console.log('Voto opcional')
+}

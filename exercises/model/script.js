fd9c5f740dde0dca03e582b417ba2f7d1bbb69b4
maxcaetanoo <maxsuelcaetano@live.com>
2024-03-01 10:38:40 -0300

    exercises model

---
+++ exercises/model/script.js
@@ -0,0 +1,6 @@
+var title = window.document.querySelector('h1#title')
+var principal = window.document.querySelector('div#principal')
+var content = window.document.querySelector('div#content')
+
+title.innerHTML = `Hora Atual`
+principal.innerHTML = `<h2>Outra coisa</h2>`
